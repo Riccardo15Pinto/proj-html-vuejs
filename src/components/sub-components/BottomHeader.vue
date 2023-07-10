ecbd2e6ffeae800d0397ae4d2e1b1876cdcb49a2
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-10 20:44:00 +0200

    add and customize bottomHeader

--- src/components/sub-components/BottomHeader.vue
+++ src/components/sub-components/BottomHeader.vue
@@ -8,8 +8,53 @@ export default {
 
 </script>
 
-<template></template>
+<template>
+    <section>
+        <div class="container">
+            <ul>
+                <li v-for="feat in features" :key="feat.title">
+                    <fontAwesomeIcon :icon=feat.image />
+                    <a href="#">{{ feat.title }}</a>
+                </li>
+            </ul>
+        </div>
+    </section>
+</template>
 
-<style lang="scss">
-@use '../src/scss/style.scss' as *;
+<style lang="scss" scoped>
+@use '../../scss/partials/vars' as *;
+
+section {
+    background-color: $blue;
+
+    .container {
+        width: $container;
+        margin: 0 auto;
+
+        ul {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            li {
+                padding: 20px 20px;
+                color: $white;
+                list-style: none;
+                font-size: 1.4rem;
+                transition: background-color 1s;
+
+                a {
+                    text-decoration: none;
+                    color: $white;
+                    padding-left: 10px;
+                    font-size: 1.1rem;
+                }
+
+                &:hover {
+                    background-color: $lightBlue;
+                }
+            }
+        }
+    }
+}
 </style>
