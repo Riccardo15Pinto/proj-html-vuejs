0782d4748becd7874bb2f03ea03ca09a76ffeb99
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-10 14:53:09 +0200

    add icons in store

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,31 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
+icons : [
 
+    {
+        typeIcon : 'facebook',
+        nameIcon: 'fa-brands fa-facebook'
+    },  
+    {
+        typeIcon : 'twitter',
+        nameIcon:'fa-brands fa-twitter'
+    },  
+    {
+        typeIcon : 'instagram',
+        nameIcon: 'fa-brands fa-instagram'
+    },
+    {
+        typeIcon : 'linkedin',
+        nameIcon: 'fa-brands fa-linkedin'
+    },  
+    {
+        typeIcon : 'google-plus',
+        nameIcon: 'fa-brands fa-google-plus'
+    }, 
+    {
+        typeIcon : 'youtub',
+        nameIcon: 'fa-brands fa-youtube'
+    }
+] 
 })
