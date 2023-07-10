656c1beae67d2742916f1e1ffc08b7ab0273693d
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-10 14:53:27 +0200

    import icons from fontawsome

--- src/main.js
+++ src/main.js
@@ -9,7 +9,12 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
 import { faFacebook } from '@fortawesome/free-brands-svg-icons'
-library.add(faFacebook)
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGooglePlus } from '@fortawesome/free-brands-svg-icons'
+import { faYoutube } from '@fortawesome/free-brands-svg-icons'
+library.add(faFacebook,faInstagram , faTwitter,faLinkedin,faGooglePlus,faYoutube)
 
 createApp(App)
 .component('fontAwesomeIcon', FontAwesomeIcon)
