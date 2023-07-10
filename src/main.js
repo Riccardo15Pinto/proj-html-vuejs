8e3270692db8c0bd63d38ebe7604e555dde3248c
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-10 15:16:51 +0200

    import solid and brand icon

--- src/main.js
+++ src/main.js
@@ -13,8 +13,20 @@ import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faGooglePlus } from '@fortawesome/free-brands-svg-icons'
+import { faPinterest } from '@fortawesome/free-brands-svg-icons'
+import { faSkype } from '@fortawesome/free-brands-svg-icons'
 import { faYoutube } from '@fortawesome/free-brands-svg-icons'
-library.add(faFacebook,faInstagram , faTwitter,faLinkedin,faGooglePlus,faYoutube)
+import { faChartLine } from '@fortawesome/free-solid-svg-icons'
+import { faPalette } from '@fortawesome/free-solid-svg-icons'
+import { faGear } from '@fortawesome/free-solid-svg-icons'
+import { faFaceSmile } from '@fortawesome/free-solid-svg-icons'
+import { faWallet } from '@fortawesome/free-solid-svg-icons'
+import { faBasketball } from '@fortawesome/free-solid-svg-icons'
+import { faBullhorn } from '@fortawesome/free-solid-svg-icons'
+import { faBriefcase } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faBullhorn ,faUser,faBriefcase ,faSkype , faPinterest , faBasketball ,faFacebook,faInstagram , faTwitter,faLinkedin,faGooglePlus,faYoutube,faChartLine,faPalette,faGear,faFaceSmile ,faWallet )
 
 createApp(App)
 .component('fontAwesomeIcon', FontAwesomeIcon)
