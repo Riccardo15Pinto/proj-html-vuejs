a430ffcaff8e935f2a4fdd8cb9264f31d0793669
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-10 20:17:18 +0200

    create bottoHeader

---
+++ src/components/sub-components/BottomHeader.vue
@@ -0,0 +1,15 @@
+<script>
+import { store } from '../../data/data'
+export default {
+    data() {
+        return store
+    }
+}
+
+</script>
+
+<template></template>
+
+<style lang="scss">
+@use '../src/scss/style.scss' as *;
+</style>
