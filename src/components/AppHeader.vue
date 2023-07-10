19b0530c76a88f8d45a164a4f2c21f937e60812a
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-10 17:35:14 +0200

    import topHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,8 @@
 <script>
-import { store } from './data/data'
+import { store } from '../data/data'
+import TopHeader from '../components/sub-components/TopHeader.vue'
 export default {
+    components: { TopHeader },
     data() {
         return store
     }
@@ -8,7 +10,11 @@ export default {
 
 </script>
 
-<template></template>
+<template>
+    <header>
+        <TopHeader />
+    </header>
+</template>
 
 <style lang="scss">
 @use '../scss/partials/vars' as *;
