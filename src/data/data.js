79053b8e49cb941dd6349d4eca5755cb0ecdb335
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-10 15:24:31 +0200

    add testimonials in store

--- src/data/data.js
+++ src/data/data.js
@@ -68,5 +68,22 @@ features:[
         title:'Office Productivity',
         image:'fa-solid fa-wallet'
     }
+],
+
+testimonials:[
+{
+    title:'Investing for Your Future',
+    nameAuthor: 'Linda Green',
+    role:'Product Manager, Apple Inc',
+    imagePath:'testimonial-1-1917x640-1-1914x639.jpg',
+    description:'It is no exaggeration to say this MasterStudy experience was transformative–both professionally and personally. This workshop will long remain a high point of my life. Thanks again…. I am feeling energized and eager to start teaching my class next week. I can’t wait to use all of my new teaching tools. I will absolutely recommend this workshop to other educators!'
+},
+{
+    title:'Paints of the Future',
+    nameAuthor: 'Xatashi Froust',
+    role:'SEO, Froust Inc',
+    imagePath:'micah-296507-unsplash-1-copy-1-1920x700-1-1917x699.jpg',
+    description:'The response to your MasterStudy has been really overwhelming! Those who participated in the workshop are spreading the word here on campus and the “buzz” is on. The VP of Instruction wants you to come back! Her goal is to have more faculty trained. She also wants to attend a workshop herself. Our President told me Masterstudy needs to be the cornerstone of our success program.”'
+},
 ]
 })
