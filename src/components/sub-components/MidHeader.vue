c89ae7140927fb2ee45636ce5bb520f92d832da5
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-10 18:40:26 +0200

    create getImageUrl

--- src/components/sub-components/MidHeader.vue
+++ src/components/sub-components/MidHeader.vue
@@ -4,6 +4,12 @@ export default {
         return {
 
         }
+    },
+    methods: {
+        getImageUrl(target) {
+            const url = new URL(`../../img/${target}`, import.meta.url);
+            return url.href
+        }
     }
 }
 </script>
