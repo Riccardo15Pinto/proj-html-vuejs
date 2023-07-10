32ae9b9ce86efe293074cc9152d10fda80eee373
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-10 15:01:53 +0200

    add features

--- src/data/data.js
+++ src/data/data.js
@@ -27,5 +27,28 @@ icons : [
         typeIcon : 'youtub',
         nameIcon: 'fa-brands fa-youtube'
     }
-] 
+],
+
+features:[
+    {
+        title:'Business',
+        image:'fa-solid fa-chart-line'
+    },
+    {
+        title:'Design',
+        image:'fa-solid fa-palette'
+    },
+    {
+        title:'Development',
+        image:'fa-solid fa-gear'
+    },
+    {
+        title:'Lifestyle',
+        image:'fa-solid fa-face-smile'
+    },
+    {
+        title:'Office Productivity',
+        image:'fa-solid fa-wallet'
+    }
+]
 })
