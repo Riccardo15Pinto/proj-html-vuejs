c86098df7f06dfb54b8843add9040e0e6d8928ed
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-11 14:40:01 +0200

    import developmentCourses

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,8 +4,9 @@ import BottomJumbo from '../components/sub-components/BottomJumbo.vue'
 import SectionInfo from '../components/sub-components/SectionInfo.vue'
 import SectionNewsletter from '../components/sub-components/SectionNewsletter.vue'
 import SectionContactUs from '../components/sub-components/SectionContactUs.vue'
+import DevelopmentCourses from './sub-components/DevelopmentCourses.vue'
 export default {
-    components: { TopJumbo, BottomJumbo, SectionInfo, SectionNewsletter, SectionContactUs },
+    components: { TopJumbo, BottomJumbo, SectionInfo, SectionNewsletter, SectionContactUs, DevelopmentCourses },
     data() {
         return {
 
@@ -17,6 +18,7 @@ export default {
 <template>
     <TopJumbo />
     <SectionInfo />
+    <DevelopmentCourses />
     <SectionNewsletter />
     <SectionContactUs />
     <BottomJumbo />
