1809ef3ace7221b1ad4a03d5663ab609c3e4241a
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-11 12:56:03 +0200

    create TopFooter

---
+++ src/components/sub-components/TopFooter.vue
@@ -0,0 +1,31 @@
+<script>
+import { store } from '../../data/data'
+export default {
+    data() {
+        return store
+    }
+}
+
+</script>
+
+<template>
+    <section>
+        <div class="container">
+
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../../scss/partials/vars' as *;
+
+section {
+    background-color: $blue;
+
+    .container {
+        width: $container;
+        margin: 0 auto;
+
+    }
+}
+</style>
