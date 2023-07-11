6100b04ce10f1387fda22db6bae2092f81a09825
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-11 14:46:36 +0200

    create CourseCard

---
+++ src/components/sub-components/CourseCard.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        item: 'object',
+    },
+    methods: {
+        getImageUrl(target) {
+            const url = new URL(`../../img/${target}`, import.meta.url);
+            return url.href
+        }
+    }
+}
+
+</script>
+
+
+<template></template>
+
+<style lang="scss" scoped>
+@use '../../scss/partials/vars' as *;
+</style>
