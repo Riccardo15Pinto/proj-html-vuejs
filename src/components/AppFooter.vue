0f4584e8c690ed54f0ac93a1c1fb606368543fcc
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-11 12:56:20 +0200

    import top and BottomFooter

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,11 +1,17 @@
 <script>
+import TopFooter from './sub-components/TopFooter.vue'
+import BottomFooter from './sub-components/BottomFooter.vue'
 export default {
+    component: { TopFooter, BottomFooter },
     data() {
 
     }
 }
 </script>
 
-<template></template>
+<template>
+    <TopFooter />
+    <BottomFooter />
+</template>
 
 <style lang="scss" scoped></style>
