4175782162d0d8dda6e59ca4df1b509563cabcd2
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-11 12:06:11 +0200

    import sectioncontactUs

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,8 +3,9 @@ import TopJumbo from '../components/sub-components/TopJumbo.vue'
 import BottomJumbo from '../components/sub-components/BottomJumbo.vue'
 import SectionInfo from '../components/sub-components/SectionInfo.vue'
 import SectionNewsletter from '../components/sub-components/SectionNewsletter.vue'
+import SectionContactUs from '../components/sub-components/SectionContactUs.vue'
 export default {
-    components: { TopJumbo, BottomJumbo, SectionInfo, SectionNewsletter },
+    components: { TopJumbo, BottomJumbo, SectionInfo, SectionNewsletter, SectionContactUs },
     data() {
         return {
 
@@ -17,6 +18,7 @@ export default {
     <TopJumbo />
     <SectionInfo />
     <SectionNewsletter />
+    <SectionContactUs />
     <BottomJumbo />
 </template>
 
