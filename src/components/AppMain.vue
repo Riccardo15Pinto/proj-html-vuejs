96286b4c81825f267eadfa8c45c38ebb180a8269
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-11 11:04:03 +0200

    import bottomjumbo

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,15 +1,19 @@
 <script>
 import TopJumbo from '../components/sub-components/TopJumbo.vue'
+import BottomJumbo from '../components/sub-components/BottomJumbo.vue'
 export default {
-    components: { TopJumbo },
+    components: { TopJumbo, BottomJumbo },
     data() {
+        return {
 
+        }
     }
 }
 </script>
 
 <template>
     <TopJumbo />
+    <BottomJumbo />
 </template>
 
 <style></style>
