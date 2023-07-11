3e6c1047f71eebc1b6910bd359af193a9c3394ce
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-11 19:19:34 +0200

    add content and set it

--- src/components/sub-components/RecentCourses.vue
+++ src/components/sub-components/RecentCourses.vue
@@ -6,13 +6,16 @@ export default {
     data() {
         return {
             store,
-            developments: [],
+            recent: [],
         }
     },
     computed: {
-        developmentCourses() {
-            const filter = store.courses.filter(element => element.category === 'Development');
-            return this.developments = filter
+        RecentCourses() {
+
+            this.recent = [...store.courses];
+            const filter = this.recent.splice(4, store.courses.length - 1);
+
+            return filter
         }
     }
 }
@@ -22,11 +25,21 @@ export default {
     <section>
         <div class="container">
 
-            <h2>Popular Development Courses</h2>
+            <h2>Recent Courses</h2>
+
+            <nav>
+                <ul>
+                    <li v-for="item in store.categories">
+                        <a href="#">{{ item.name }}</a>
+                    </li>
+                </ul>
+            </nav>
 
             <div class="card-container">
-                <CourseCard v-for="n in developmentCourses" :item="n" :key="n.id" />
+                <CourseCard v-for="n in RecentCourses" :item="n" :key="n.id" />
             </div>
+
+            <button>SHOW ALL</button>
         </div>
     </section>
 </template>
@@ -44,15 +57,71 @@ section {
             color: $blue;
             margin-bottom: 40px;
             margin-left: 10px;
+            text-align: center;
+        }
+
+        nav {
+            width: 1220px;
+            margin: 35px auto;
+
+            ul {
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+
+                li {
+                    list-style: none;
+
+                    a {
+                        text-decoration: none;
+                        color: $blue;
+                        padding: 10px 20px;
+                        background-color: transparent;
+                        border-radius: 30px;
+
+                        &:hover {
+                            background-color: $blue;
+                            color: $white;
+
+                        }
+
+                        &:focus {
+                            background-color: $blue;
+                            color: $white;
+
+                        }
+                    }
+                }
+            }
         }
 
         .card-container {
             display: flex;
             align-items: center;
+            flex-wrap: wrap;
 
             .card {
-                margin: 0 10px;
+                margin: 10px 10px;
                 flex-basis: calc(100% / 6 - 20px);
+            }
+        }
+
+        button {
+            display: block;
+            margin: 40px auto;
+            padding: 20px 40px;
+            border-radius: 30px;
+            color: white;
+            font-weight: bolder;
+            border: none;
+            background: linear-gradient(to right, $Darkred 50%, $red 50%);
+            background-size: 200% 100%;
+            background-position: right bottom;
+            transition: all .5s ease-out;
+            cursor: pointer;
+
+            &:hover {
+                background-position: left bottom;
             }
         }
     }
