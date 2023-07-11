6fa133c6139ff246f591f3e4ad5188b96d2e6c60
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-11 10:28:32 +0200

    add content and customize it

--- src/components/sub-components/TopJumbo.vue
+++ src/components/sub-components/TopJumbo.vue
@@ -4,13 +4,30 @@ export default {
     data() {
         return {
             store,
+            index: 0,
+        }
+    },
+    computed: {
+        currentIndex() {
+            return this.index
         }
     },
     methods: {
         getImageUrl(target) {
             const url = new URL(`../../img/${target}`, import.meta.url);
             return url.href
+        },
+
+        getImageSlider() {
+            this.index++
+            if (this.index === store.activities.length) return this.index = 0
+            else if (!this.index) return this.index++
+            setInterval(this.getImageSlider, 5000)
         }
+
+    },
+    mounted() {
+        // this.getImageSlider()
     }
 }
 </script>
@@ -18,13 +35,16 @@ export default {
 <template>
     <section>
         <div class="container">
-            <div>
-                <h2></h2>
-                <p></p>
+            <div class="jumbo-text">
+                <div class="container-text">
+                    <h2>{{ store.activities[currentIndex].title }}</h2>
+                    <p>{{ store.activities[currentIndex].text }}</p>
+                </div>
             </div>
-            <div>
+            <div class="band"></div>
+            <div class="jumbo-image">
                 <figure>
-                    <img src="" alt="">
+                    <img :src="getImageUrl(store.activities[currentIndex].image)" alt="">
                 </figure>
             </div>
         </div>
@@ -33,4 +53,65 @@ export default {
 
 <style lang="scss" scoped>
 @use '../../scss/partials/vars' as *;
+
+section {
+    padding: 20px 0;
+
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+        height: 400px;
+
+        .jumbo-text {
+            background-color: $red;
+            height: 100%;
+            flex-basis: 40%;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            position: relative;
+
+            .container-text {
+                color: $white;
+                position: absolute;
+                z-index: 1;
+                left: 270px;
+
+                h2 {
+                    font-size: 3rem;
+                    padding-bottom: 20px
+                }
+
+                p {
+                    font-size: 1.3rem;
+                }
+            }
+        }
+
+        .band {
+            background-color: $red;
+            position: absolute;
+            transform: skewX(28deg);
+            width: 254px;
+            height: 100%;
+            left: 655px;
+        }
+
+        .jumbo-image {
+            flex-basis: 60%;
+            height: 100%;
+
+            figure {
+                height: 100%;
+
+                img {
+                    height: 100%;
+                }
+            }
+        }
+    }
+}
 </style>
