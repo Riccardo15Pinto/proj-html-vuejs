3ed23921e6e59222549d1d2f3b329304dd41c7ca
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-12 00:21:51 +0200

    import Popolarcourses

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,8 +6,9 @@ import SectionNewsletter from '../components/sub-components/SectionNewsletter.vu
 import SectionContactUs from '../components/sub-components/SectionContactUs.vue'
 import DevelopmentCourses from './sub-components/DevelopmentCourses.vue'
 import RecentCourses from './sub-components/RecentCourses.vue'
+import PopolarCourses from './sub-components/PopolarCourses.vue'
 export default {
-    components: { TopJumbo, BottomJumbo, SectionInfo, SectionNewsletter, SectionContactUs, DevelopmentCourses, RecentCourses },
+    components: { TopJumbo, BottomJumbo, SectionInfo, SectionNewsletter, SectionContactUs, DevelopmentCourses, RecentCourses, PopolarCourses },
     data() {
         return {
 
@@ -23,6 +24,7 @@ export default {
         <SectionInfo />
         <RecentCourses />
         <SectionNewsletter />
+        <PopolarCourses />
         <SectionContactUs />
         <BottomJumbo />
     </main>
