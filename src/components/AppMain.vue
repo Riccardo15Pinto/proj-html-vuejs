b1ac1f36a393c61c2e5db24060f4218e79cc2a50
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-11 08:54:18 +0200

    create app main

---
+++ src/components/AppMain.vue
@@ -0,0 +1,15 @@
+<script>
+import TopJumbo from '../components/sub-components/TopJumbo.vue'
+export default {
+    components: { TopJumbo },
+    data() {
+
+    }
+}
+</script>
+
+<template>
+    <TopJumbo />
+</template>
+
+<style></style>
