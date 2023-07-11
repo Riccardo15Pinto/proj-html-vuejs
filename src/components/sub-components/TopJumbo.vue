8c6de8b93cbb7111cf7ce838a0e0d02d6b90c921
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-11 08:59:06 +0200

    add template and import store

--- src/components/sub-components/TopJumbo.vue
+++ src/components/sub-components/TopJumbo.vue
@@ -1,7 +1,10 @@
 <script>
+import { store } from '../../data/data'
 export default {
     data() {
-
+        return {
+            store,
+        }
     },
     methods: {
         getImageUrl(target) {
@@ -12,6 +15,22 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <section>
+        <div class="container">
+            <div>
+                <h2></h2>
+                <p></p>
+            </div>
+            <div>
+                <figure>
+                    <img src="" alt="">
+                </figure>
+            </div>
+        </div>
+    </section>
+</template>
 
-<style></style>
+<style lang="scss" scoped>
+@use '../../scss/partials/vars' as *;
+</style>
