edf3b340940e2008b206a9f67da2553b71770fd2
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-11 12:05:54 +0200

    create sectioncontacus and import contactcard

---
+++ src/components/sub-components/SectionContactUs.vue
@@ -0,0 +1,21 @@
+<script>
+import { store } from '../../data/data'
+import ContactCard from './ContactCard.vue'
+export default {
+    components: { ContactCard },
+    data() {
+        return store
+    },
+}
+</script>
+
+<template>
+    <section>
+        <ContactCard />
+        <ContactCard />
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../../scss/partials/vars' as *;
+</style>
