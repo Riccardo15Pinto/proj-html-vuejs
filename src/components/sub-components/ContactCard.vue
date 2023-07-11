4590f3f47621eab44e6a0070f62fcf19b6e2b151
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-11 12:05:33 +0200

    create contacard

---
+++ src/components/sub-components/ContactCard.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        getImageUrl(target) {
+            const url = new URL(`../../img/${target}`, import.meta.url);
+            return url.href
+        }
+    }
+}
+</script>
+
+<template>
+    <section>
+
+
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../../scss/partials/vars' as *;
+</style>
